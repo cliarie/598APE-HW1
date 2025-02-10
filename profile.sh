c023d4721b80be67d34ca00c96464daf04327f46
Claire <clarrechen@gmail.com>
2025-02-10 05:20:20 -0600

    Automated profiling

---
+++ profile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FLAMEGRAPH_DIR="$HOME/flamegraph"
+
+echo "Starting dtrace profiling.."
+sudo dtrace -n 'profile-997 /execname == "main.exe"/ { @[ustack()] = count(); }' -o out.stacks &
+
+echo "Running main.exe.."
+./main.exe -i inputs/pianoroom.ray --ppm -o output/pianoroom.ppm -H 500 -W 500
+
+sleep 4
+
+echo "Making flamegraph.."
+"$FLAMEGRAPH_DIR/stackcollapse.pl" out.stacks | "$FLAMEGRAPH_DIR/flamegraph.pl" > flamegraph.svg
+
+echo "Opening flamegraph.."
+open flamegraph.svg
