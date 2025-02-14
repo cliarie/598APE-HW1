9ac3cb2326c74a50385d93de5e3dc9017d289e03
Claire <clarrechen@gmail.com>
2025-02-13 21:41:38 -0600

    Add inlining, rid ternary operator for fabs

--- src/box.cpp
+++ src/box.cpp
@@ -3,21 +3,22 @@
 Box::Box(const Vector &c, Texture* t, double ya, double pi, double ro, double tx, double ty):Plane(c, t, ya, pi, ro, tx, ty){}
 Box::Box(const Vector &c, Texture* t, double ya, double pi, double ro, double tx):Plane(c, t, ya, pi, ro, tx,tx){}
 
-double Box::getIntersection(Ray ray){
+inline double Box::getIntersection(Ray ray){
    double time = Plane::getIntersection(ray);
-   Vector dist = solveScalers(right, up, vect, ray.point+ray.vector*time-center);
-   if(time==inf) 
-      return time;
-   return ( ((dist.x>=0)?dist.x:-dist.x)>textureX/2 || ((dist.y>=0)?dist.y:-dist.y)>textureY/2 )?inf:time;
+   if (time == inf) return time;
+   Vector hitpoint = ray.point+ray.vector*time-center;
+   Vector dist = solveScalers(right, up, vect, hitpoint);
+   return (fabs(dist.x)> textureX * 0.5 || fabs(dist.y)>textureY * 0.5 )?inf:time;
 }
 
-bool Box::getLightIntersection(Ray ray, double* fill){
+inline bool Box::getLightIntersection(Ray ray, double* fill){
    const double t = ray.vector.dot(vect);
    const double norm = vect.dot(ray.point)+d;
    const double r = -norm/t;
    if(r<=0. || r>=1.) return false;
-   Vector dist = solveScalers(right, up, vect, ray.point+ray.vector*r-center);
-   if( ((dist.x>=0)?dist.x:-dist.x)>textureX/2 || ((dist.y>=0)?dist.y:-dist.y)>textureY/2 ) return false;
+   Vector hitpoint = ray.point+ray.vector*r-center;
+   Vector dist = solveScalers(right, up, vect, hitpoint);
+   if (fabs(dist.x)>textureX * 0.5 || fabs(dist.y)>textureY * 0.5) return false;
 
    if(texture->opacity>1-1E-6) return true;   
    unsigned char temp[4];
